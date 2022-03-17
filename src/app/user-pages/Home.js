c4309fd92adb494785e3b28f257c0da7e9307d19
Mehmet yıldız <yildiz655@gmail.com>
2022-03-17 20:26:15 +0300

    first start

---
+++ src/app/user-pages/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import "./Home.css"
+export class Home extends Component {
+    render() {
+        return (
+            <div className='mainboard'>
+                <div className='bg'>
+                    <img className="bg_logo" src={require("../../assets/images/carousel/banner_7.jpg")} alt="carousel item" />
+                </div>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+                <p>testttt</p>
+            </div>
+        )
+    }
+}
+
+export default Home
